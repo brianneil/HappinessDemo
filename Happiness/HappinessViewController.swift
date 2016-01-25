e7a644848ae5a3447f4711409c2499f7f8e8d5d0
Brian Neil <Brian@Brians-MacBook-Pro.local>
2016-01-25 12:36:45 -0800

    First commit
    Just getting the first commit in place

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Brian Neil on 1/22/16.
+//  Copyright © 2016 Apollo Hearing. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+
+}
